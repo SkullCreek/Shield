634c9025d4abe939168b30bbc248a1a0b64368d1
SkullCreek <darpan.bahadur.2001@gmail.com>
2021-10-24 16:46:50 +0530

    Done with sidebar html and css

---
+++ script/script.js
@@ -0,0 +1,14 @@
+/*started sidebar coding*/
+function sidebar(){
+    var sidebar=document.getElementById("slidebar");
+    var aside=document.getElementById("animation");
+    sidebar.style.display="block";
+    aside.style.width="350px";
+    aside.style.animation = "slidebar 0.1s";
+    aside.style.animationFillMode = "forwards";
+}
+function sidebar_close(){
+    var sidebar=document.getElementById("slidebar");
+    sidebar.style.display="none";
+}
+/*ended sidebar coding*/
